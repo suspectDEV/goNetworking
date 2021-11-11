30fa5676ac16eeababe6137083a4a63c60411913
suspectDEV <alexander.forerolopez@gmail.com>
2021-11-10 23:03:01 -0500

    chapter 3 finished

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	UDPDaytimeClient()
+	Ping()
 }
 
 /*	IP
@@ -243,11 +243,11 @@ func ListenTCP() {
 /*	UDPDaytimeClient
  */
 func UDPDaytimeClient() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
 		os.Exit(1)
 	}
-	service := os.Args[1]
+	service := os.Args[2]
 	udpAddr, err := net.ResolveUDPAddr("udp4", service)
 	checkError(err)
 
@@ -267,6 +267,96 @@ func UDPDaytimeClient() {
 
 /*
 	END UDPDaytimeClient */
+
+/*	UDPDaytimeServer
+ */
+func UDPtimeServer() {
+	service := ":1200"
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+	checkError(err)
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	checkError(err)
+
+	for {
+		handleClient(conn)
+		fmt.Println("client-connected")
+	}
+}
+
+func handleClient(conn *net.UDPConn) {
+	var buf [512]byte
+	_, addr, err := conn.ReadFromUDP(buf[0:])
+	if err != nil {
+		return
+	}
+	daytime := time.Now().String()
+	conn.WriteToUDP([]byte(daytime), addr)
+}
+
+/*
+	END UDPDaytimeClient */
+
+/*	Ping
+ */
+func Ping() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "host")
+		os.Exit(1)
+	}
+	// addr, err := net.ResolveIPAddr("ip", os.Args[1])
+	// if err != nil {
+	// 	fmt.Println("Resolution error", err.Error())
+	// 	os.Exit(1)
+	// }
+	// conn, err := net.DialIP("ip4:icmp", addr, addr)
+	// ---
+	// Comento código original del libro.
+
+	service := os.Args[1]
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+
+	var msg [512]byte
+	msg[0] = 8
+	msg[1] = 0
+	msg[2] = 0
+	msg[3] = 0
+	msg[4] = 0
+	msg[5] = 13
+	msg[6] = 0
+	msg[7] = 37
+	len := 8
+
+	check := checkSum(msg[0:len])
+	msg[2] = byte(check >> 8)
+	msg[3] = byte(check & 255)
+	_, err = conn.Write(msg[0:len])
+	checkError(err)
+
+	_, err = conn.Read(msg[0:])
+	checkError(err)
+
+	fmt.Println("Got response")
+	if msg[5] == 13 {
+		fmt.Println("identifier matches")
+	}
+	if msg[7] == 37 {
+		fmt.Println("Sequence matches")
+	}
+	os.Exit(0)
+}
+
+func checkSum(msg []byte) uint16 {
+	sum := 0
+	for n := 1; n < len(msg)-1; n += 2 {
+		sum += int(msg[n])*256 + int(msg[n+1])
+	}
+	sum = (sum >> 16) + (sum & 0xffff)
+	sum += (sum >> 16)
+	var answer uint16 = uint16(^sum)
+	return answer
+}
 
 func checkError(err error) {
 	if err != nil {
